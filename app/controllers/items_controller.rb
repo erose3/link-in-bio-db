1eff2a73bf9c0c8ff500f9288f42172685f05a15
erose3 <eli.rosenthal3@gmail.com>
2025-04-28 16:54:10 +0000

    formatting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   
   def index
+    @list_of_items = Item.all.order({ :link_description => :asc })
+    
     render({ :template => "item_templates/index" })
   end
 
